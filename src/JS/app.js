17f62f9b793f673dc54cde90ddb55c4891c3f7df
Lucca Nunes <luccanunesgit@gmail.com>
2020-07-24 20:03:28 -0300

    put everything inside setup and changed verify button

--- src/JS/app.js
+++ src/JS/app.js
@@ -8,7 +8,7 @@ const questionsPerSection = divs.length / sections.length;
 const stripe = document.querySelector(".freebirdSolidBackground");
 const bg_color = getComputedStyle(stripe).backgroundColor;
 const submit = document.querySelector('.freebirdFormviewerViewNavigationButtons');
-
+const content = document.querySelector('.freebirdFormviewerViewFormContent');
 
 const interval = setInterval(() => {
     if (texts) {
@@ -23,16 +23,54 @@ function setup() {
     const ad = document.createElement('h3');
     const verify = document.createElement('div');
 
+    document.getElementsByClassName("freebirdThemedFilledButtonM2")[0].style.backgroundColor = bg_color;
+    submit.style.marginRight = '445px'
+
     ad.innerHTML = 'Risk extension created by Pedro Queiroz & Lucca Nunes';
     footer.appendChild(ad);
 
     verify.textContent = 'Verificar';
     verify.style.backgroundColor = bg_color;
     verify.classList.add('button');
-    verify.style.alignSelf = 'flex-start'
-    verify.style.margin = '0'
+    verify.style.alignSelf = 'flex-start';
+    verify.style.margin = '0';
     // bottomDiv.appendChild(verify);
     bottomDiv.insertBefore(verify, submit);
+
+    const buttons = document.getElementsByClassName('button');
+    const radios = document.getElementsByClassName('appsMaterialWizToggleRadiogroupEl');
+
+    verify.onclick = () => {
+        let count = 0;
+        let answered = new Array(sections.length);
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].classList.contains('isChecked')) {
+                count++;
+                answered[Math.floor(i / questionsPerSection)] = 1;
+            }
+        }
+        let wrong = false;
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].classList.contains('isChecked') && buttons[i].textContent == 'Desfazer') {
+                wrong = true;
+                alert(`Opa! A questão ${Math.floor(i / questionsPerSection) + 1} está respondida mas a alternativa escolhida está riscada...`)
+            }
+        }
+        if (!wrong) {
+            if (count === sections.length) {
+                alert('Tudo certo!');
+            } else {
+                string = '';
+                for (let i = 0; i < sections.length; i++) {
+                    if (typeof (answered[i]) == 'undefined') {
+                        string += i + 1 + ', ';
+                    }
+                }
+                string = string.substring(0, string.length - 2);
+                alert(`As seguintes questões não foram respondidas: ${string}`);
+            }
+        }
+    }
 
     for (let i = 0; i < divs.length; i++) {
         if (i % questionsPerSection == 0) {
@@ -66,38 +104,5 @@ function setup() {
     }
 }
 
-const buttons = document.getElementsByClassName('button');
-const radios = document.getElementsByClassName('appsMaterialWizToggleRadiogroupEl');
 
-verify.onclick = () => {
-    let count = 0;
-    let answered = new Array(sections.length);
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].classList.contains('isChecked')) {
-            count++;
-            answered[Math.floor(i / questionsPerSection)] = 1;
-        }
-    }
-    let wrong = false;
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].classList.contains('isChecked') && buttons[i].textContent == 'Desfazer') {
-            wrong = true;
-            alert(`Opa! A questão ${Math.floor(i / questionsPerSection) + 1} está respondida mas a alternativa escolhida está riscada...`)
-        }
-    }
-    if (!wrong) {
-        if (count === sections.length) {
-            alert('Tudo certo!');
-        } else {
-            string = '';
-            for (let i = 0; i < sections.length; i++) {
-                if (typeof (answered[i]) == 'undefined') {
-                    string += i + 1 + ', ';
-                }
-            }
-            string = string.substring(0, string.length - 2);
-            alert(`As seguintes questões não foram respondidas: ${string}`);
-        }
-    }
-}
 
